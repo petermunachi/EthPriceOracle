0e978f60c6ee839b8965ad2a958fe6b4cdcc20cf
peter munachi <alimunachipeter@gmail.com>
2021-01-12 13:53:18 +0100

    creation of smart contract oracle

---
+++ oracle/contracts/CallerContractInterface.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.4.22 <0.8.0;
+
+abstract contract CallerContractInterface {
+
+  function callback(uint256 _ethPrice, uint256 id) public virtual;
+}
